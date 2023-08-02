dacc28d1b49a742b240a5ca8147e4ddfb42c22c4
EthanSherr <esherrthan@gmail.com>
2023-08-02 16:22:34 -0400

    save and go

--- 8_1_pointers/main.cpp
+++ 8_1_pointers/main.cpp
@@ -6,10 +6,11 @@ int main() {
 
     // * on the left is a pointer
     int* intPointer = &five;
-    //                & on the right is a pointer reference
+    //                & on the right is a pointer reference, go from a value to a pointer to that value!
 
     (*intPointer)++;
 
+    //                  on the right side, use * to get the value, in this case it's assigned to a new int dereference variable
     int dereference = *intPointer;
 
     dereference++;
@@ -18,6 +19,8 @@ int main() {
     std::cout << "five: " << five << ", dereference: " << dereference << std::endl;
     
     std::cout << "second test:\n";
+    
+    // writing directly to intPointer memory location to change five
     (*intPointer) = 52;
     std::cout << "five: " << five << ", dereference: " << dereference << std::endl;
 
