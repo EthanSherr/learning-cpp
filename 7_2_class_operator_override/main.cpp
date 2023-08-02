79f56b004b092e59446e5b42c3651345e3eca1cb
Ethan Sherr <esherrthan@gmail.com>
2023-08-01 22:43:02 -0400

    learning about overload

---
+++ 7_2_class_operator_override/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ImaginaryNumber.hpp"
+
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+
+    ImaginaryNumber x(1.0f, 2.0f);
+
+    cout << "x: " << x << endl;
+    return 0;
+}
